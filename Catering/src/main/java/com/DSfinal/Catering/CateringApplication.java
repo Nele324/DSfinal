1e14dd7993f296831b6a430fa7b98ab1dbe641f8
nele <nele.lauryssen@student.kuleuven;be>
2026-04-24 12:11:16 +0200

    initial commit

---
+++ Catering/src/main/java/com/DSfinal/Catering/CateringApplication.java
@@ -0,0 +1,13 @@
+package com.DSfinal.Catering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CateringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CateringApplication.class, args);
+	}
+
+}
---
+++ Venue/src/test/java/com/DSfinal/Venue/VenueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DSfinal.Venue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VenueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
