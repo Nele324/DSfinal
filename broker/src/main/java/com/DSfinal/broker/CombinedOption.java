8a82cb0c05c1e47faabc4b9ce484c234d76028ee
nsthe <naing.sumonthein@student.kuleuven.be>
2026-05-08 18:45:34 +0200

    sumon commit

--- broker/src/main/java/com/DSfinal/broker/CombinedOption.java
+++ broker/src/main/java/com/DSfinal/broker/CombinedOption.java
@@ -7,6 +7,8 @@ public class CombinedOption {
     private String cateringName;
     private double totalPrice;
     private boolean available;
+    private int cateringMaxGuests;
+    private int venueMaxGuests;
 
     public CombinedOption() {}
 
@@ -17,19 +19,32 @@ public class CombinedOption {
         this.cateringName = catering.getName();
         this.totalPrice = venue.getPricePerDay() + catering.getPricePerPerson();
         this.available = venue.isAvailable() && catering.isAvailable();
+        this.cateringMaxGuests = catering.getMaxGuests();
+        this.venueMaxGuests= venue.getCapacity();
     }
 
     // Getters en Setters (nodig voor JSON!)
     public String getVenueId() { return venueId; }
     public void setVenueId(String venueId) { this.venueId = venueId; }
+
     public String getCateringId() { return cateringId; }
     public void setCateringId(String cateringId) { this.cateringId = cateringId; }
+
     public String getVenueName() { return venueName; }
     public void setVenueName(String venueName) { this.venueName = venueName; }
+
     public String getCateringName() { return cateringName; }
     public void setCateringName(String cateringName) { this.cateringName = cateringName; }
+
     public double getTotalPrice() { return totalPrice; }
     public void setTotalPrice(double totalPrice) { this.totalPrice = totalPrice; }
+
     public boolean isAvailable() { return available; }
     public void setAvailable(boolean available) { this.available = available; }
+
+    public int getCateringMaxGuests() { return cateringMaxGuests; }
+    public void setCateringMaxGuests(int cateringMaxGuests) {this.cateringMaxGuests = cateringMaxGuests;}
+
+    public int getVenueMaxGuests() { return venueMaxGuests; }
+    public void setVenueMaxGuests(int venueMaxGuests) { this.venueMaxGuests = venueMaxGuests; }
 }
--- broker/src/main/java/com/DSfinal/broker/VenueHall.java
+++ broker/src/main/java/com/DSfinal/broker/VenueHall.java
@@ -3,23 +3,26 @@ package com.DSfinal.broker;
 public class VenueHall {
     private String id;
     private String name;
+    private int capacity;
     private double pricePerDay;
     private boolean available;
 
     // Getters en Setters
     public String getId() { return id; }
     public String getName() { return name; }
+    public int getCapacity() { return capacity; }
     public double getPricePerDay() { return pricePerDay; }
     public boolean isAvailable() { return available; }
 
     public void setId(String id) { this.id = id; }
     public void setName(String name) { this.name = name; }
+    public void setCapacity(int capacity) { this.capacity = capacity; }
     public void setPricePerDay(double pricePerDay) { this.pricePerDay = pricePerDay; }
     public void setAvailable(boolean available) { this.available = available; }
 
     @Override
     public String toString() {
-        return "VenueHall{name='" + name + "', price=" + pricePerDay + ", available=" + available + "}";
+        return "VenueHall{name='" + name + "', price=" + pricePerDay + "',capacity='" + capacity + ", available=" + available + "}";
     }
 }
 
