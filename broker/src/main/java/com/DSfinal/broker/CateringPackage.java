b306e115179fdf5a92c3d1a152151c28554ed624
nele <nele.lauryssen@student.kuleuven;be>
2026-05-10 16:54:00 +0200

    full working catering connected to broker

--- broker/src/main/java/com/DSfinal/broker/CateringPackage.java
+++ broker/src/main/java/com/DSfinal/broker/CateringPackage.java
@@ -5,23 +5,27 @@ public class CateringPackage {
     private String name;
     private int maxGuests;
     private double pricePerPerson;
-    private boolean available;
+    private String status;
 
     // Getters en Setters
     public String getId() { return id; }
     public String getName() { return name; }
     public int getMaxGuests() { return maxGuests; }
     public double getPricePerPerson() { return pricePerPerson; }
-    public boolean isAvailable() { return available; }
+    public String getStatus() { return status; }
+
+    public boolean isAvailable() {
+        return "AVAILABLE".equals(status);
+    }
 
     public void setId(String id) { this.id = id; }
     public void setName(String name) { this.name = name; }
     public void setMaxGuests(int maxGuests) { this.maxGuests = maxGuests; }
     public void setPrice(double pricePerPerson) { this.pricePerPerson = pricePerPerson; }
-    public void setAvailable(boolean available) { this.available = available; }
+    public void setStatus(String status) { this.status = status; }
 
     @Override
     public String toString() {
-        return "CateringPackage{name='" + name + "', price=" + pricePerPerson + "',maxGuests='"+ maxGuests+ ", available=" + available + "}";
+        return "CateringPackage{name='" + name + "', price=" + pricePerPerson + "',maxGuests='"+ maxGuests+ ", status=" + status + "}";
     }
 }
